c5ecc70989b6c10fedb285b6a73b4cde1579fa56
AlySemsem <69532035+AlySemsem@users.noreply.github.com>
2022-05-25 21:09:36 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,11 +1,11 @@
 public class Main {
     public static void main(String[] args) throws Exception {
         Mutex m = new Mutex();
-        Scheduler sc = new Scheduler(m, 2);
+        Scheduler sc = new Scheduler(m, 3);
 
-        Interpeter.readProgram("Program_1.txt", m, sc, 0);        
-        Interpeter.readProgram("Program_2.txt", m, sc, 1);
-        Interpeter.readProgram("Program_3.txt", m, sc, 4);
+        Interpeter.readProgram("Program_1.txt", m, sc, 4);        
+        Interpeter.readProgram("Program_2.txt", m, sc, 0);
+        Interpeter.readProgram("Program_3.txt", m, sc, 10);
         sc.run();
 
 
